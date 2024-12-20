2fcecd50977e3230ea873db8ddabb6faaf44fa76
Sahil Mandre <sahilmandre@gmail.com>
2024-12-20 15:12:27 +0530

    Images and Grids added + Gradient animation added

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 function Grid() {
-  return <div>Grid</div>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            title,
+            description,
+            className,
+            img,
+            id,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
 }
 
 export default Grid;
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { cn } from "@/utils/cn";
 
 export const BentoGrid = ({
@@ -10,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -23,25 +24,60 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
+  id,
+  img,
+  imgClassName,
+  spareImg,
+  titleClassName,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  id: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: " rgb(2,0,36)",
+        backgroundColor:
+          " linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(27,26,34,1) 24%, rgba(34,34,34,1) 51%, rgba(23,77,108,1) 100%, rgba(0,212,255,1) 100%)",
+      }}
     >
-      {header}
+      <div className={`${id === 6} && 'flex justify-center h-full' `}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(className, "object-cover object-center")}
+            />
+          )}
+        </div>
+        <div
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          }`}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className={"object-cover object-center h-full w-full"}
+            />
+          )}
+        </div>
+      </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
