c6f9d9ca59619dcaa6be35416f32c56687b9eb43
Sahil Mandre <sahilmandre@gmail.com>
2024-12-21 13:05:16 +0530

    Project section completed , More UI edits will be made later

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { projects } from "@/data";
+import { PinContainer } from "./ui/3d-pin";
+import { cover } from "three/src/extras/TextureUtils.js";
+import { FaLocationArrow } from "react-icons/fa";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        A small selection of <span className="text-purple"> projects</span>
+      </h1>
+
+      {/* Project Cards */}
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div
+            key={id}
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[500px] w-[90vw]"
+          >
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-[480px] w-[80vw] h-[30vh] sm:h-[40vh] ">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <img src="/bg.png" alt="bg.png" />
+                </div>
+                <img src={img} alt={title} className="z-10 bottom-0 absolute" />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center  ">
+                  {iconLists.map((icon, index) => (
+                    <div
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center"
+                      key={icon}
+                      style={{
+                        transform: `translateX(-${5 * index * 2} px)`,
+                      }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
